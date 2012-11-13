acb4ea73a01346321cad6c9e197783ffa6d7bcea
vadia007 <vaaadik@mail.ru>
2012-11-13 22:00:59 +0200

    header

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-11-11 16:51:24 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2012-11-11T16:51:24+00:00	INFO	joomlafailure	Пустой пароль не допускается
+2012-11-12T00:00:45+00:00	INFO	joomlafailure	Пустой пароль не допускается
